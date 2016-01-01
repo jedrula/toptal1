6d729b18bb7ce0edd64a467114a3f7ce763622f9
Andrzej Swaton <andrzej.swaton@gmail.com>
2016-01-01 16:11:40 +0100

    changed names to show its the client part of the app

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Project-andrzej-swaton
+# Project-andrzej-swaton-cli
 
 This README outlines the details of collaborating on this Ember application.
 A short introduction of this app could easily go here.
--- config/environment.js
+++ config/environment.js
@@ -2,7 +2,7 @@
 
 module.exports = function(environment) {
   var ENV = {
-    modulePrefix: 'project-andrzej-swaton',
+    modulePrefix: 'project-andrzej-swaton-cli',
     environment: environment,
     baseURL: '/',
     locationType: 'auto',
