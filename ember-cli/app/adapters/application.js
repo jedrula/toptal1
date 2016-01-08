4b9105ebb2c24b623f0429559f1f54c7e40f6219
Andrzej Swaton <andrzej.swaton@gmail.com>
2016-01-08 01:37:58 +0100

    breaking change - start of jsonapi implementation with jsonapify in the backend and JSONAPI ember adapter and serializer

--- ember-cli/app/adapters/application.js
+++ ember-cli/app/adapters/application.js
@@ -2,7 +2,7 @@ import DS from 'ember-data';
 import ENV from 'project-andrzej-swaton-cli/config/environment';
 import DataAdapterMixin from 'ember-simple-auth/mixins/data-adapter-mixin';
 
-export default DS.RESTAdapter.extend(DataAdapterMixin, {
+export default DS.JSONAPIAdapter.extend(DataAdapterMixin, {
   //authorizer: 'authorizer:application'
   authorizer: 'authorizer:token',
   host: ENV.APP.API_SERVER_URL,
--- ember-cli/app/serializers/application.js
+++ ember-cli/app/serializers/application.js
@@ -1,5 +1,5 @@
 import DS from 'ember-data';
 
-export default DS.RESTSerializer.extend({
+export default DS.JSONAPISerializer.extend({
   primaryKey: '_id'
 });
